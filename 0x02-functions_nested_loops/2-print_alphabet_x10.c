5363ddbe3863683a58406d4e2fffd437b1cb9e32
Nashwa <n7216069@gmail.com>
2023-09-12 09:06:28 -0700

    “N”

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/*prints 10 times the alphabet, in lowercase by using _putchar function*/
+
+void print_alphabet_x10(void);
+{
+	int i;
+	int j;
+
+	for (j = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j)
+		}
+		_putchar('\n');
+	}
+}
+
+
