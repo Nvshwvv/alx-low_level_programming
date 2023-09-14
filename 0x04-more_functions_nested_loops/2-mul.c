748e37fe1af050d982391884e92fc6a873d68f6a
Nashwa <n7216069@gmail.com>
2023-09-14 16:05:51 -0700

    a

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multplay two numbers
+ * @x: The first number to multplay
+ * @y: The second number to multplay
+ * Return: The ruslt of multplay x and y
+*/
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
