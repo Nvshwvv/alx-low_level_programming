bddba39a63178e6dec9ca839f5223c82cb68ea3e
Nashwa <n7216069@gmail.com>
2023-09-25 13:46:31 -0700

    aaaaaa

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strstr - function that locates a substring
+ * @haystack: input
+ * @needle: input also
+ * Return: 0 (Success)
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *na = haystack;
+		char *sh = needle;
+
+		while (*na == *sh && *sh != '\0')
+		{
+			na++;
+			sh++;
+		}
+		if (*sh == '\0')
+			return (haystack);
+	}
+	return (NULL);
+}
