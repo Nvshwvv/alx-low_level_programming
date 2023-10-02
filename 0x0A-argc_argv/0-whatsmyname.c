a2b8b3be573069d9788c951671feae1b9e88e121
Nashwa <n7216069@gmail.com>
2023-10-02 07:22:32 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: argument counter
+ * @argv: list
+ * Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", *argv[]);
+	return (0);
+}
