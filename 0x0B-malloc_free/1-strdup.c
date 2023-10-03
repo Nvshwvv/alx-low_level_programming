1bb8cc29914472724c3a0b8d3d874a97988532fc
Nashwa <n7216069@gmail.com>
2023-10-03 08:57:00 -0700

    l

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup -  returns a pointer to a newly allocated space in memory
+ * @str: pointer to string
+ * Return: null or pointer
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *n;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[size] != '\0'; size++)
+		;
+	n = malloc(size * sizeof(*str) + 1);
+	if (m == 0)
+		return (NULL);
+
+	{
+		for (; i < size; i++)
+			n[i] = str[i];
+	}
+	return (n);
+}
