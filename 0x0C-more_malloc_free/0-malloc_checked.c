921aa8c7341ab0016229b73f88eb1ec240fa928b
Nashwa <n7216069@gmail.com>
2023-10-08 08:04:03 -0700

    j

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
  * malloc_checked - allocates memory using malloc.
@@ -16,9 +15,6 @@ void *malloc_checked(unsigned int b)
 	d = malloc(b);
 
 		if (d == 0)
-		{
 			exit(98);
-
-		}
 	return (d);
 }
