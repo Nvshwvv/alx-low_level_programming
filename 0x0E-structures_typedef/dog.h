7880efc9bec1b64d94f2ee188e641d5ee0daf932
Nashwa <n7216069@gmail.com>
2023-10-10 16:13:19 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog strucutuer
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ * Description: a dog strucutuer that contain detals about dog
+*/
+
+strcut dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
