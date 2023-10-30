1fd6985f073120a0f91aed563e51058f40096fb6
nashwa <n7216069@example.com>
2023-10-30 10:31:08 -0700

    fixed bugs

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,15 +11,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes;
-	char buffer[READ_BUF_SIZE * 8];
+	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	bytes = read(fd, &buffer[0], letters);
-	bytes = write(STDOUT_FILENO, &buffer[0], bytes);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
 	return (bytes);
 }
